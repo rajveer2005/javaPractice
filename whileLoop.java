286170a2a0d1b6ad27250a0687ff58e7381f99b2
rajveer jaddhav <jadhavrajveer86@gmail.com>
2026-03-14 23:21:27 +0530

    Initial Java practice code

---
+++ whileLoop.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class whileLoop {
+    public static void main(String args[]){
+        System.out.printf("hello").printf("word").println("byy");
+        
+        // NUMBERS FROM 1 TO 10
+        // int counter = 1;
+        // while(counter <= 100){
+        //     System.out.print(counter + " ");
+        //     counter++;
+        // }
+
+
+
+        // numbers  from 1 to n
+
+        // Scanner SC = new Scanner(System.in);
+
+        // int range = SC.nextInt();
+        // int counter = 1;
+        // while(counter<= range){
+        //         System.out.print(counter  + " ");
+        //         counter ++;
+        // }
+        // System.out.println("");
+
+
+        // SUM OF FIRST N NATURAL NUMBER
+
+        // Scanner SC = new Scanner(System.in);
+
+        // int n = SC.nextInt();
+        // int sum = 0;
+        // int i = 1;
+        // while(i <= n){
+        //     sum = sum+i;
+        //     i++;
+            
+        // }
+        //     System.out.println("sum is: "+ sum);
+
+        
+    }
+}
