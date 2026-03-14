27c0e0e99e26856b3e56f2f40843722ae46b9f35
rajveer jaddhav <jadhavrajveer86@gmail.com>
2026-03-14 23:38:43 +0530

    practice done

---
+++ practice.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+// Write a Java program that declares variables for:
+// Name (String)
+// Age (int)
+// Height (float)
+// IsStudent (boolean)
+
+public class practice{
+    public static void main(String[] args) {
+    //    String name = "rajveer";
+    //    int age = 20;
+    //    float height = 5.3f;
+    //    boolean IsStudent = true;
+
+    //    System.out.println("name:" + name);
+    //           System.out.println("age:" + age);
+    //    System.out.println("height:" + height);
+    //    System.out.println("isstudent:" + IsStudent);
+     
+
+
+//      Write a program to take two numbers and print:
+// Sum Difference Product Division
+        Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Difference = " + (a - b));
+        System.out.println("Product = " + (a * b));
+        System.out.println("Division = " + (a / b));
+
+
+    }
+}
