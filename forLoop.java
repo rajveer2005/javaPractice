eeae3a1aa806542391f07188b15d72e528b84732
rajveer jaddhav <jadhavrajveer86@gmail.com>
2026-03-17 22:52:24 +0530

    code update

--- forLoop.java
+++ forLoop.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 
 public class forLoop{
     public static void main(String args[]){
-       
+        // for(int i =1; i<=10; i++){
+        //     System.out.println("hello world");
+        // }
 
 
 //      PRINT SQUARE PATTERN
