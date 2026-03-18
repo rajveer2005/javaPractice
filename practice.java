18489b14a7459426bcee1e127088014750489320
rajveer jaddhav <jadhavrajveer86@gmail.com>
2026-03-18 23:10:15 +0530

    code add

--- practice.java
+++ practice.java
@@ -2,9 +2,8 @@
 import java.util.Scanner;
 
 // Write a Java program that declares variables for:
-// Name (String)
-// Age (int)
-
+// Height (float)
+// IsStudent (boolean)
 
 public class practice{
     public static void main(String[] args) {
